5d4792371e6b727b4750299268eea05ca441d8dc
shahil-mohammed-ms <115145954+shahil-mohammed-ms@users.noreply.github.com>
2024-02-09 14:34:31 +0530

    wishlist

--- src/wishlist/wishlist.controller.ts
+++ src/wishlist/wishlist.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Body, Delete, Param } from '@nestjs/common';
+import { WishlistService } from './wishlist.service';
+import { Wishlists } from './wishlist.entity';
 
 @Controller('wishlist')
-export class WishlistController {}
+export class WishlistController {
+  constructor(private readonly wishlistService: WishlistService) {}
+
+  @Post()
+  async createWishlist(@Body() wishlistData): Promise<any> {
+    return this.wishlistService.createWishlist(wishlistData);
+  }
+
+  @Get()
+  async getAllWishlists(): Promise<any> {
+    return this.wishlistService.getAllWishlists();
+  }
+
+  @Delete(':id')
+  async deleteWishlist(@Param('id') wishlistId: string) {
+    await this.wishlistService.deleteWishlist(wishlistId);
+    return { message: 'Wishlist deleted successfully' };
+  }
+
+}
--- src/wishlist/wishlist.service.ts
+++ src/wishlist/wishlist.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Wishlists } from './wishlist.entity';
 
 @Injectable()
-export class WishlistService {}
+export class WishlistService {
+  constructor(
+    @InjectRepository(Wishlists)
+    private readonly wishlistRepository: Repository<Wishlists>,
+  ) {}
+
+  async createWishlist(wishlistData): Promise<any> {
+    const wishlist = this.wishlistRepository.create(wishlistData);
+    return await this.wishlistRepository.save(wishlist);
+  }
+
+  async getAllWishlists(): Promise<Wishlists[]> {
+    return await this.wishlistRepository.find({ relations: ['product'] });
+  }
+
+  async deleteWishlist(wishlistId: string): Promise<void> {
+    await this.wishlistRepository.delete(wishlistId);
+  }
+
+}
